0cbd33f121baef385571562b9a4f78b8b63f4176
Makar Riabcev <makar.riabcev@nxp.com>
2020-04-03 01:40:42 +0200

    almost finalized assignment - needs check

--- Algorithms 1/percolation/Percolation.java
+++ Algorithms 1/percolation/Percolation.java
@@ -4,6 +4,7 @@
  *  Description: Algorithms 1: Percolation - Week 1
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -20,31 +21,79 @@ public class Percolation {
     public Percolation(int n) {
         if (n > 0) {
             gridLine = n;
-            gridSize = Math.pow(n, 2);
+            gridSize = n * n;
             numberOfOpenSites = 0;
             top = gridSize;
             bottom = gridSize + 1;
             wqufTop = new WeightedQuickUnionUF(gridSize + 1);
             wqufBottom = new WeightedQuickUnionUF(gridSize + 2);
+            /*
+            for (int i = 1; i <= gridLine; i++) {
+                wqufTop.union(0, getCellGrid(1, i));
+                wqufBottom.union(0, getCellGrid(1, i));
+                wqufBottom.union(gridSize + 1, getCellGrid(gridLine, i));
+            }
+            */
         }
-
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        if (isExistOnGrid(row, col)) {
 
+            openedGrid[getCellGrid(row, col)] = true;
+            if (isOpen(row, col)) {
+
+                if (row == 1) {
+                    wqufBottom.union(top, getCellGrid(row, col));
+                    wqufTop.union(top, getCellGrid(row, col));
+                }
+
+                if (row == gridLine) {
+                    wqufBottom.union(bottom, getCellGrid(row, col));
+                }
+                // check - open Left
+                if (isExistOnGrid(row, col - 1) && isOpen(row, col - 1)) {
+                    wqufBottom.union(getCellGrid(row, col), getCellGrid(row, col - 1));
+                    wqufTop.union(getCellGrid(row, col), getCellGrid(row, col - 1));
+                }
+
+                // check - open Right
+                if (isExistOnGrid(row, col + 1) && isOpen(row, col + 1)) {
+                    wqufBottom.union(getCellGrid(row, col), getCellGrid(row, col + 1));
+                    wqufTop.union(getCellGrid(row, col), getCellGrid(row, col + 1));
+                }
+
+                // check - open Up
+                if (isExistOnGrid(row - 1, col) && isOpen(row - 1, col)) {
+                    wqufBottom.union(getCellGrid(row, col), getCellGrid(row - 1, col));
+                    wqufTop.union(getCellGrid(row, col), getCellGrid(row - 1, col));
+                }
+
+                // check - open Down
+                if (isExistOnGrid(row + 1, col) && isOpen(row + 1, col)) {
+                    wqufBottom.union(getCellGrid(row, col), getCellGrid(row + 1, col));
+                    wqufTop.union(getCellGrid(row, col), getCellGrid(row + 1, col));
+                }
+
+                numberOfOpenSites++;
+                openedGrid[getCellGrid(row, col)] = true;
+            }
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        checkBorders(row, col);
-        return openedGrid[pid(row, col)];
+        if (isExistOnGrid(row, col)) {
+            return openedGrid[getCellGrid(row, col)];
+        }
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        checkBorders(row, col);
-        return isOpen(row, col) && wqufTop.connected(0, pid(row, col));
+        if (isExistOnGrid(row, col)) {
+            return isOpen(row, col) && wqufTop.connected(0, getCellGrid(row, col));
+        }
     }
 
     // returns the number of open sites
@@ -71,9 +120,27 @@ public class Percolation {
         return true;
     }
 
+    private int getCellGrid(int row, int col) {
+        return (row - 1) * gridLine + col;
+    }
+
     // test client (optional)
     public static void main(String[] args) {
-
+        int size = Integer.parseInt(args[0]);
+        argCount = args.length;
+        Percolation percolation = new Percolation(size);
+        for (int i = 0; args.length >= 2; i += 2) {
+            int row = Integer.parseInt(args[i]);
+            int col = Integer.parseInt(args[i + 1]);
+            percolation.open(row, col);
+            if (percolation.percolates()) {
+                StdOut.printf("%n it percolates %n");
+            }
+            argCount -= 2;
+        }
+        if (!percolation.percolates()) {
+            StdOut.print("doesnt percolate");
+        }
     }
 
 
--- Algorithms 1/percolation/PercolationStats.java
+++ Algorithms 1/percolation/PercolationStats.java
@@ -4,35 +4,67 @@
  *  Description: Algorithms 1: Percolation - Week 1
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private int trials;
+    private double[] results;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+        if (n > 0 && trials > 0) {
+            int gridLine = n;
+            int gridSize = n * n;
+            trials = trials;
+            results = new double[trials];
 
+            for (int i = 0; i < trials; i++) {
+                Percolation percolation = new Percolation(gridLine);
+                while (!percolation.percolates()) {
+                    int row = StdRandom.uniform(1, gridLine + 1);
+                    int col = StdRandom.uniform(1, gridLine + 1);
+                    percolation.open(row, col);
+                }
+                int numberOfOpenSites = percolation.numberOfOpenSites();
+                results[trials] = (double) numberOfOpenSites / gridSize;
+            }
+        }
     }
 
     // sample mean of percolation threshold
     public double mean() {
-
+        return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-
+        return StdStats.stddev(results);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-
+        return mean() - ((1.96 * stddev()) / Math.sqrt(trials));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-
+        return mean() + ((1.96 * stddev()) / Math.sqrt(trials));
     }
 
     // test client (see below)
     public static void main(String[] args) {
-
+        int gridLine = 9;
+        int trials = 0;
+        if (args.length >= 2) {
+            gridLine = Integer.parseInt(args[0]);
+            trials = Integer.parseInt(args[1]);
+        }
+        PercolationStats percolationStats = new PercolationStats(gridLine, trials);
+        String confidence = ps.confidenceLo() + ", " + ps.confidenceHi();
+        StdOut.println("mean                    = " + ps.mean());
+        StdOut.println("stddev                  = " + ps.stddev());
+        StdOut.println("95% confidence interval = " + confidence);
     }
 }
